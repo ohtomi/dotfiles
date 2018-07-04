65974aea427cd19dd462387884c3c2d6da4cbe39
ohtomi <ohtomi.kenichi@gmail.com>
2018-07-04 21:30:05 +0900

    Remove bookmarklet to scrap to todo project

--- bookmarklet/scrap-to-todo.js
+++
@@ -1,7 +0,0 @@
-javascript: (function () {
-    var title = window.prompt('Scrap to todo.', document.title);
-    if (!title) return;
-    var lines = [window.location.href];
-    var body = encodeURIComponent(lines.join('\n'));
-    window.open('https://scrapbox.io/ohtomi-todo/' + encodeURIComponent(title.trim()) + '?body=' + body);
-})()
