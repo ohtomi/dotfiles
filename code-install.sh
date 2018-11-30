2b328d41601d4772f729b3833ebbe099fbda47a4
ohtomi <ohtomi.kenichi@gmail.com>
2018-11-30 16:15:15 +0900

    Add code-install.sh to install vscode extensions

---
+++ code-install.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+code --install-extension kenhowardpdx.vscode-gist
+code --install-extension MS-CEINTL.vscode-language-pack-ja
+code --install-extension ms-kubernetes-tools.vscode-kubernetes-tools
+code --install-extension msjsdiag.debugger-for-chrome
+code --install-extension PeterJausovec.vscode-docker
+code --install-extension redhat.vscode-yaml
+code --install-extension Shan.code-settings-sync
+code --install-extension vscoss.vscode-ansible
+
+echo
+code --list-extensions
