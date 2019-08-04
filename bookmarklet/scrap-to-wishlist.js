9f139869ac03122172f1d65b87e9a7ccd0794792
ohtomi <ohtomi.kenichi@gmail.com>
2019-08-04 21:54:26 +0900

    Change target project from wishlist to journal

--- bookmarklet/scrap-to-wishlist.js
+++ bookmarklet/scrap-to-wishlist.js
@@ -1,7 +1,7 @@
 javascript: (function () {
     var title = window.prompt('Scrap to wishlist.', document.title);
     if (!title) return;
-    var lines = [window.location.href];
+    var lines = [` [${window.location.href} ${title}]`];
     var body = encodeURIComponent(lines.join('\n'));
-    window.open('https://scrapbox.io/ohtomi-wishlist/' + encodeURIComponent(title.trim()) + '?body=' + body);
+    window.open(`https://scrapbox.io/ohtomi-journal/読みたい本?body=${body}`);
 })()
