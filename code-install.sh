febb485b4d2b22f46292d8e9cbe41ea3e547da6a
ohtomi <ohtomi.kenichi@gmail.com>
2019-08-18 20:13:59 +0900

    Install ms-vscode-remote.* as vscode extension

--- code-install.sh
+++ code-install.sh
@@ -4,8 +4,14 @@ code --install-extension kenhowardpdx.vscode-gist
 code --install-extension mechatroner.rainbow-csv
 code --install-extension MS-CEINTL.vscode-language-pack-ja
 code --install-extension ms-kubernetes-tools.vscode-kubernetes-tools
+code --install-extension ms-azuretools.vscode-docker
+code --install-extension ms-vscode-remote.remote-containers
+code --install-extension ms-vscode-remote.remote-ssh
+code --install-extension ms-vscode-remote.remote-ssh-edit
+code --install-extension ms-vscode-remote.remote-ssh-explorer
+code --install-extension ms-vscode-remote.remote-wsl
+code --install-extension ms-vscode-remote.vscode-remote-extensionpack
 code --install-extension msjsdiag.debugger-for-chrome
-code --install-extension PeterJausovec.vscode-docker
 code --install-extension redhat.vscode-yaml
 code --install-extension Shan.code-settings-sync
 code --install-extension vscoss.vscode-ansible
