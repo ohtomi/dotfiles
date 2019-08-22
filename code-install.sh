6484b0ae3bfc1b0d60041e86268c5363b06912b5
ohtomi <ohtomi.kenichi@gmail.com>
2019-08-22 23:40:57 +0900

    Install vscode extensions for toml, rst and remove for WSL

--- code-install.sh
+++ code-install.sh
@@ -1,15 +1,16 @@
 #!/usr/bin/env bash
 
+code --install-extension bungcip.better-toml
 code --install-extension kenhowardpdx.vscode-gist
+code --install-extension lextudio.restructuredtext
 code --install-extension mechatroner.rainbow-csv
+code --install-extension ms-azuretools.vscode-docker
 code --install-extension MS-CEINTL.vscode-language-pack-ja
 code --install-extension ms-kubernetes-tools.vscode-kubernetes-tools
-code --install-extension ms-azuretools.vscode-docker
 code --install-extension ms-vscode-remote.remote-containers
 code --install-extension ms-vscode-remote.remote-ssh
 code --install-extension ms-vscode-remote.remote-ssh-edit
 code --install-extension ms-vscode-remote.remote-ssh-explorer
-code --install-extension ms-vscode-remote.remote-wsl
 code --install-extension ms-vscode-remote.vscode-remote-extensionpack
 code --install-extension msjsdiag.debugger-for-chrome
 code --install-extension redhat.vscode-yaml
