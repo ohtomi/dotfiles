8981155877e984130336b41756482dc7405d2eb7
ohtomi <ohtomi.kenichi@gmail.com>
2021-03-14 21:30:08 +0900

    Dont call poetry config when zsh start

--- setup.sh
+++ setup.sh
@@ -13,21 +13,19 @@ if [ ! -f "$HOME/$extra_dotfile" ]; then
   echo "created $HOME/$extra_dotfile ! set GITHUB_TOKEN"
 fi
 
-extra_dotfile=".scrapbox_token"
-if [ ! -f "$HOME/$extra_dotfile" ]; then
-  echo "export SCRAPBOX_TOKEN=(my token)" >$HOME/$extra_dotfile
-  echo "created $HOME/$extra_dotfile ! set SCRAPBOX_TOKEN"
-fi
-
 
 mkdir -p $HOME/bin
 
-binfiles="gzipinfo fortune"
+binfiles=(gzipinfo fortune)
 
 for binfile in $binfiles
 do
   ln -sfn "$HOME/src/github.com/ohtomi/dotfiles/bin/$binfile" $HOME/bin
 done
+
+
+mkdir -p $HOME/Library/Application\ Support/pypoetry
+ln -sfn "$HOME/src/github.com/ohtomi/dotfiles/pypoetry/config.toml" $HOME/Library/Application\ Support/pypoetry
 
 
 mkdir -p $HOME/.lazybones
