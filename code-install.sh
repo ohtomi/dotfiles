b79f5fc93be7dd238c53d4c5a74f5bdb906e40de
ohtomi <ohtomi.kenichi@gmail.com>
2025-04-19 10:41:30 +0900

    Remove unused tools

--- code-install.sh
+++ code-install.sh
@@ -1,26 +1,19 @@
 #!/usr/bin/env zsh
 
-code --install-extension bungcip.better-toml
-code --install-extension GitHub.vscode-pull-request-github
-code --install-extension kenhowardpdx.vscode-gist
-code --install-extension lextudio.restructuredtext
-code --install-extension mauve.terraform
-code --install-extension mechatroner.rainbow-csv
+code --install-extension docker.docker
+code --install-extension eamodio.gitlens
+code --install-extension editorconfig.editorconfig
+code --install-extension github.vscode-pull-request-github
 code --install-extension ms-azuretools.vscode-docker
-code --install-extension MS-CEINTL.vscode-language-pack-ja
-code --install-extension ms-kubernetes-tools.vscode-kubernetes-tools
-code --install-extension ms-python.python
+code --install-extension ms-ceintl.vscode-language-pack-ja
 code --install-extension ms-vscode-remote.remote-containers
 code --install-extension ms-vscode-remote.remote-ssh
 code --install-extension ms-vscode-remote.remote-ssh-edit
 code --install-extension ms-vscode-remote.remote-ssh-explorer
-code --install-extension ms-vscode-remote.remote-wsl
 code --install-extension ms-vscode-remote.vscode-remote-extensionpack
 code --install-extension ms-vscode.github-issues-prs
-code --install-extension msjsdiag.debugger-for-chrome
-code --install-extension redhat.vscode-yaml
-code --install-extension Shan.code-settings-sync
-code --install-extension vscoss.vscode-ansible
+code --install-extension ms-vscode.remote-explorer
+code --install-extension ms-vscode.remote-server
 
 echo
 code --list-extensions
--- setup.sh
+++ setup.sh
@@ -7,12 +7,6 @@ do
   ln -sfn "$HOME/src/github.com/ohtomi/dotfiles/$dotfile" $HOME
 done
 
-extra_dotfile=".github_token"
-if [ ! -f "$HOME/$extra_dotfile" ]; then
-  echo "export GITHUB_TOKEN=(my token)" >$HOME/$extra_dotfile
-  echo "created $HOME/$extra_dotfile ! set GITHUB_TOKEN"
-fi
-
 
 mkdir -p $HOME/bin
 
@@ -22,11 +16,3 @@ for binfile in $binfiles
 do
   ln -sfn "$HOME/src/github.com/ohtomi/dotfiles/bin/$binfile" $HOME/bin
 done
-
-
-mkdir -p $HOME/Library/Application\ Support/pypoetry
-ln -sfn "$HOME/src/github.com/ohtomi/dotfiles/pypoetry/config.toml" $HOME/Library/Application\ Support/pypoetry
-
-
-mkdir -p $HOME/.lazybones
-ln -sfn "$HOME/src/github.com/ohtomi/dotfiles/.lazybones/managed-config.json" $HOME/.lazybones
