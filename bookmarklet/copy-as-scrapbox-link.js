13392972b3e9b34e983958ad259aedd970dad374
ohtomi <ohtomi.kenichi@gmail.com>
2026-01-17 16:02:53 +0900

    Remove bookmarklet files

--- bookmarklet/copy-as-scrapbox-link.js
+++
@@ -1,9 +0,0 @@
-(function () {
-    var scrapboxLink = '[' + window.location.href + ' ' + document.title + ']';
-    var textarea = document.createElement('textarea');
-    textarea.textContent = scrapboxLink;
-    document.body.appendChild(textarea);
-    textarea.select();
-    document.execCommand('copy');
-    document.body.removeChild(textContent);
-})()
--- bookmarklet/scrap-to-wishlist.js
+++
@@ -1,7 +0,0 @@
-(function () {
-    var title = window.prompt('Scrap to wishlist.', document.title);
-    if (!title) return;
-    var lines = [` [${window.location.href} ${title}]`];
-    var body = encodeURIComponent(lines.join('\n'));
-    window.open(`https://scrapbox.io/ohtomi-journal/読みたい本?body=${body}`);
-})()
